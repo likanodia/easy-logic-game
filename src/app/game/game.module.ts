9c94f506f393ebe3fba8ff8bf2ca0a8f498f8a2a
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-12-04 22:02:24 +0400

    Add styles

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -5,6 +5,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatBadgeModule} from '@angular/material/badge';
+
+
 
 @NgModule({
   declarations: [GameBoardComponent],
@@ -14,6 +20,10 @@ import { FormsModule } from '@angular/forms';
     MatGridListModule,
     MatIconModule,
     FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatBadgeModule
   ],
   exports: [GameBoardComponent],
 })
