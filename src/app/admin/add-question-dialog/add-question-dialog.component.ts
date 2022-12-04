b353261b1128313139edcf915398f9f1c1c4f2e9
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-12-04 13:25:16 +0400

    Close dialog after question addition

--- src/app/admin/add-question-dialog/add-question-dialog.component.ts
+++ src/app/admin/add-question-dialog/add-question-dialog.component.ts
@@ -1,40 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { INewQuestion, Question } from '../question';
 import { QuestionsService } from '../questions.service';
-
 
 @Component({
   selector: 'app-add-question-dialog',
   templateUrl: './add-question-dialog.component.html',
-  styleUrls: ['./add-question-dialog.component.scss']
+  styleUrls: ['./add-question-dialog.component.scss'],
 })
 export class AddQuestionDialogComponent implements OnInit {
   questionForm = new FormGroup({
     answer: new FormControl('', [Validators.required]),
     firstPicture: new FormControl('', [Validators.required]),
     secondPicture: new FormControl('', [Validators.required]),
-  })
-  constructor(
-    private questionService : QuestionsService ) {
+  });
+  constructor(private questionService: QuestionsService, private dialog:MatDialog)  {}
 
-     }
-
-  ngOnInit(): void {
-
-  }
-  onAddQuestion(){
+  ngOnInit(): void {}
+  onAddQuestion() {
     console.log(this.questionForm.value);
     if (this.questionForm.valid) {
-      this.questionService.addQuestion(this.questionForm.value as INewQuestion).subscribe((data)=>{
-        alert('Your question added');
-        this.questionForm.reset();
-      })
-      error: () =>{
+      this.questionService
+        .addQuestion(this.questionForm.value as INewQuestion)
+        .subscribe((data) => {
+          this.questionForm.reset();
+          this.dialog.closeAll();
+        });
+      error: () => {
         alert('ERR');
-      }
-      
+      };
     }
-    
   }
 }
