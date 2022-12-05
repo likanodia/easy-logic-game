855af47ea4471a8dd11ab250dca63f73e0086e73
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-12-06 01:50:37 +0400

    Add routing to auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
+import { NotfoundComponent } from './auth/notfound/notfound.component';
+import { HeaderComponent } from './header/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+  { path: 'admin', component: HeaderComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
