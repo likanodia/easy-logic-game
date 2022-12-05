76f1caa4ef8ac4a04f7ce185370451e5826d5d7c
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-12-06 01:45:22 +0400

    Add registration form

--- src/app/auth/regitration/regitration.component.ts
+++ src/app/auth/regitration/regitration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-regitration',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./regitration.component.scss']
 })
 export class RegitrationComponent implements OnInit {
+  hide: any;
+  registrationForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    admin: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get name() {
+    return this.registrationForm.get('name') as FormControl;
+  }
+  get lastname() {
+    return this.registrationForm.get('lastname') as FormControl;
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getErrorMessagePass() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.password.hasError('password') ? 'Not a valid password' : '';
+  }
+
+  getErrorMessageName() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.password.hasError('password') ? 'Not a valid name' : '';
+  }
+  getErrorMessageLastName() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.password.hasError('password') ? 'Not a valid lastname' : '';
   }
 
 }
