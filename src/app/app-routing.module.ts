78feb07bc9604bdc8c755de12c9277def7164b66
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-12-06 19:35:54 +0400

    Create auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { AuthGuard } from './auth/auth.guard';
 import { AuthComponent } from './auth/auth/auth.component';
 import { NotfoundComponent } from './auth/notfound/notfound.component';
 import { GameBoardComponent } from './game/game-board/game-board.component';
@@ -18,6 +19,7 @@ const routes: Routes = [
       { path: 'leaderboard', component: LeaderboardComponent },
       { path: 'admin', component: AdminPanelComponent },
     ],
+    canActivate: [AuthGuard],
   },
 
   { path: '**', component: NotfoundComponent },
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,32 +3,31 @@ import { Router } from '@angular/router';
 import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private router: Router) { }
-  setToken(token: string): void{
-    localStorage.setItem('token', token)
+  constructor(private router: Router) {}
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
   }
 
-  getToken(): string | null{
+  getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  isLoggedIn(){
+  isLoggedIn(): boolean {
     return this.getToken() !== null;
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('token');
-    this.router.navigate(['login'])
+    this.router.navigate(['login']);
   }
 
-  login({email, password}: any): Observable<any> {
+  login({ email, password }: any): Observable<any> {
     if (email === 'admin@gmail.com' && password === '123') {
       this.setToken('admin');
-      return of({name: 'admin', email: 'admin@gmail.com' });
+      return of({ name: 'admin', email: 'admin@gmail.com' });
     }
     return throwError(new Error('Failed to Login'));
   }
